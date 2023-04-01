757927e32dc72c8d76dc0834bc3f22f489583522
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-01 21:06:12 +0200

    Complete `Sum of Multiples` exercise

--- sum-of-multiples/sum-of-multiples.jl
+++ sum-of-multiples/sum-of-multiples.jl
@@ -1,2 +1,22 @@
 function sum_of_multiples(limit, factors)
+  unique = BitSet()
+
+  for factor in factors
+    if factor == 0 
+      continue
+    end
+
+    multiple = factor
+
+    while multiple < limit
+      push!(unique, multiple)
+
+      # On each iteration, add the factor, i.e.
+      # multiple = 3 + 3 -> 6 + 3 -> 9 + 3…
+      multiple += factor
+    end
+
+  end
+
+  sum(unique)
 end
