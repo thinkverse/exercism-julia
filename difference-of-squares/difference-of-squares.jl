651e6d431b7873dfc9794a871ec7ead31b40dc67
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-04 07:20:20 +0200

    Complete `Difference of Squares` exercise

--- difference-of-squares/difference-of-squares.jl
+++ difference-of-squares/difference-of-squares.jl
@@ -1,14 +1,8 @@
 "Square the sum of the first `n` positive integers"
-function square_of_sum(n)
-
-end
+square_of_sum(n::Int)::Int = sum(0:n)^2
 
 "Sum the squares of the first `n` positive integers"
-function sum_of_squares(n)
-
-end
+sum_of_squares(n::Int)::Int = sum(n -> n^2, 0:n)
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
-function difference(n)
-
-end
+difference(n::Int)::Int = square_of_sum(n) - sum_of_squares(n)
