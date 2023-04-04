81f9d8384ca96e64db8e16cb3dbc7c58c1bc1b6a
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-04 14:47:00 +0200

    Complete `Secret Handshake` exercise

--- secret-handshake/secret-handshake.jl
+++ secret-handshake/secret-handshake.jl
@@ -1,3 +1,25 @@
 function secret_handshake(code)
+  strings = []
 
+  if code | 0b00001 == code
+		push!(strings, "wink")
+	end
+
+  if code | 0b00010 == code
+		push!(strings, "double blink")
+	end
+
+  if code | 0b00100 == code
+		push!(strings, "close your eyes")
+	end
+
+  if code | 0b01000 == code
+		push!(strings, "jump")
+	end
+
+  if code | 0b10000 == code
+		reverse!(strings)
+	end
+
+  return strings
 end
