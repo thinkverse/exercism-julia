eb94ee2ad5c05e5f64c1ed5ecdacd7d80a773c49
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-05 13:12:32 +0200

    Complete `Word Count` exercise

--- word-count/word-count.jl
+++ word-count/word-count.jl
@@ -1,3 +1,10 @@
 function wordcount(sentence)
+  words = Dict()
 
+  for m in eachmatch(r"[a-z]+'[a-z]+|[0-9a-z]+", lowercase(sentence))
+    # https://en.wikibooks.org/wiki/Introducing_Julia/Dictionaries_and_sets#Simple_example:_counting_words
+    words[m.match] = get(words, m.match, 0) + 1
+  end
+
+  return words
 end
